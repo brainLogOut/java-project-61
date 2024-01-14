226757c00b12810fc1253090bcdce67b9050cfb5
Alexey Vasilev <davetmsq@gmail.com>
2024-01-14 22:42:27 +0500

    step4: linter fix on Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -9,7 +9,7 @@ public class Cli {
         System.out.println("Welcome to the Brain Games");
         System.out.println("May I have your name?");
         String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!" );
+        System.out.println("Hello, " + userName + "!");
         scanner.close();
     }
 }
