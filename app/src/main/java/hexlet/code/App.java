35f9b85ce534e5175954e40e2485e64dbb5ac210
Alexey Vasilev <davetmsq@gmail.com>
2024-01-15 20:59:39 +0500

    step5: Add Even.java & link on asciinema

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        String gameNumber = scanner.next();
+        System.out.println("Your choice: " + gameNumber);
+        switch (gameNumber) {
+            case "0":
+                break;
+            case "1":
+                Cli.greeting();
+                break;
+            case "2":
+                Even.evenGame();
+                break;
+            default:
+                System.out.println("Incorrect game number");
+                break;
+        }
+        scanner.close();
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,32 @@
+package hexlet.code;
+
+import java.util.Scanner;
+public class Even {
+    public static void evenGame() {
+        Scanner scanner = new Scanner(System.in);
+        int validAnswers = 0;
+
+        System.out.println("Welcome to the Brain Games");
+        System.out.println("May I have your name?");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        do {
+            int currentNumber = (int) (Math.random() * 100);
+            System.out.println("Question: " + currentNumber);
+            String answer = scanner.next();
+            if ((answer.equalsIgnoreCase("yes")
+                    && currentNumber % 2 == 0)
+                    || (answer.equalsIgnoreCase("no")
+                    && currentNumber %2 != 0)) {
+                validAnswers++;
+            } else break;
+        } while (validAnswers < 3);
+        if (validAnswers == 3) {
+            System.out.println("Congratulations, " + userName + "!");
+        } else {
+            System.out.println("Wrong answer, try again later");
+        }
+        scanner.close();
+    }
+}
