815eecd4629caf0bb64b3daf8c066dfc53b34d2c
Alexey Vasilev <davetmsq@gmail.com>
2024-01-22 22:21:16 +0500

    Edit Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -12,10 +12,15 @@ public class Even {
         for (int i = 0; i < Engine.getMaxRounds(); i++) {
             final int maxNumberValue = 100;
             int currentNumber = Engine.genNumber(maxNumberValue);
+            String result = "";
 
             System.out.println("Question: " + currentNumber);
             String answer = scanner.next();
-            String result = calcResult(currentNumber);
+            if (currentNumber % 2 == 0) {
+                result = "yes";
+            } else if (currentNumber % 2 != 0) {
+                result = "no";
+            }
             String checkResult = Engine.check(answer, result, (i + 1));
             if (checkResult.equals("error")) {
                 return 1;
@@ -25,14 +30,14 @@ public class Even {
         return 0;
     }
 
-    private static String calcResult(int currentNumber) {
-        String cResult = "";
-
-        if (currentNumber % 2 == 0) {
-            cResult = "yes";
-        } else if (currentNumber % 2 != 0) {
-            cResult = "no";
-        }
-        return cResult;
-    }
+//    private static String calcResult(int currentNumber) {
+//        String cResult = "";
+//
+//        if (currentNumber % 2 == 0) {
+//            cResult = "yes";
+//        } else if (currentNumber % 2 != 0) {
+//            cResult = "no";
+//        }
+//        return cResult;
+//    }
 }
