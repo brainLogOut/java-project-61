2fca871e5aaeb49331439f82561eade6f0a2d40f
Alexey Vasilev <davetmsq@gmail.com>
2024-01-26 20:54:08 +0500

    Edit App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -50,7 +50,7 @@ public class App {
                 Prime.callGame();
                 break;
             default:
-                System.out.println("Incorrect game number");
+                System.out.println("Incorrect game number!");
                 break;
         }
     }
